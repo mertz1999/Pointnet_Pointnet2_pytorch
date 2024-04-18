87bf2b569035d69bfdef82b7665110fbb4123c89
Reza Tanakizadeh <47104040+mertz1999@users.noreply.github.com>
2024-04-18 12:34:08 +0330

    Update train_classification.py

--- train_classification.py
+++ train_classification.py
@@ -2,229 +2,130 @@
 Author: Benny
 Date: Nov 2019
 """
-
-import os
-import sys
-import torch
-import numpy as np
-
-import datetime
-import logging
-import provider
-import importlib
-import shutil
-import argparse
-
-from pathlib import Path
-from tqdm import tqdm
 from data_utils.ModelNetDataLoader import ModelNetDataLoader
+import argparse
+import numpy as np
+import os
+import torch
+import logging
+from tqdm import tqdm
+import sys
+import importlib
+from torchvision import transforms
+import utils
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+import seaborn as sns
+
+
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 ROOT_DIR = BASE_DIR
 sys.path.append(os.path.join(ROOT_DIR, 'models'))
 
+
 def parse_args():
     '''PARAMETERS'''
-    parser = argparse.ArgumentParser('training')
+    parser = argparse.ArgumentParser('Testing')
     parser.add_argument('--use_cpu', action='store_true', default=False, help='use cpu mode')
     parser.add_argument('--gpu', type=str, default='0', help='specify gpu device')
     parser.add_argument('--batch_size', type=int, default=24, help='batch size in training')
-    parser.add_argument('--model', default='pointnet_cls', help='model name [default: pointnet_cls]')
     parser.add_argument('--num_category', default=40, type=int, choices=[10, 40],  help='training on ModelNet10/40')
-    parser.add_argument('--epoch', default=200, type=int, help='number of epoch in training')
-    parser.add_argument('--learning_rate', default=0.001, type=float, help='learning rate in training')
     parser.add_argument('--num_point', type=int, default=1024, help='Point Number')
-    parser.add_argument('--optimizer', type=str, default='Adam', help='optimizer for training')
-    parser.add_argument('--log_dir', type=str, default=None, help='experiment root')
-    parser.add_argument('--decay_rate', type=float, default=1e-4, help='decay rate')
+    parser.add_argument('--model', type=str, required=True, help='model path')
     parser.add_argument('--use_normals', action='store_true', default=False, help='use normals')
-    parser.add_argument('--process_data', action='store_true', default=False, help='save data offline')
     parser.add_argument('--use_uniform_sample', action='store_true', default=False, help='use uniform sampiling')
+    parser.add_argument('--num_votes', type=int, default=3, help='Aggregate classification scores with voting')
     return parser.parse_args()
 
 
-def inplace_relu(m):
-    classname = m.__class__.__name__
-    if classname.find('ReLU') != -1:
-        m.inplace=True
+def test(model, loader, num_class=40, vote_num=1):
+    test_time_augmentation = transforms.Compose([
+                                utils.RandRotation_z(),
+                                utils.RandomNoise(),
+                                utils.ToTensor()
+                             ])
+    # list of class names
+    classes = [line.rstrip() for line in open('./modelnet40_normal_resampled/modelnet40_shape_names.txt')]
 
-
-def test(model, loader, num_class=40):
     mean_correct = []
-    class_acc = np.zeros((num_class, 3))
     classifier = model.eval()
+    class_acc = np.zeros((num_class, 3))
+
+    total_labels = []
+    total_predic = []
+
 
     for j, (points, target) in tqdm(enumerate(loader), total=len(loader)):
+        if not args.use_cpu: device = 'cuda:0'
+        else: device = 'cpu'
+    
+        pred, _ = classifier(points.transpose(2, 1).to(device))
 
-        if not args.use_cpu:
-            points, target = points.cuda(), target.cuda()
+        # test time aug
+        for _ in range(vote_num):
+          aug_data = points.clone()
+          for i in range(pred.shape[0]):
+            aug_data[i] = test_time_augmentation(aug_data[i])
 
-        points = points.transpose(2, 1)
-        pred, _ = classifier(points)
+          aug_out, _ = classifier(aug_data.transpose(2, 1).to(device))
+          pred += aug_out
+
+        pred /= vote_num+1
         pred_choice = pred.data.max(1)[1]
 
-        for cat in np.unique(target.cpu()):
-            classacc = pred_choice[target == cat].eq(target[target == cat].long().data).cpu().sum()
-            class_acc[cat, 0] += classacc.item() / float(points[target == cat].size()[0])
-            class_acc[cat, 1] += 1
+        target = target.cuda()
+        total_labels += list(target.detach().cpu().numpy())
+        total_predic += list(pred_choice.detach().cpu().numpy())
 
         correct = pred_choice.eq(target.long().data).cpu().sum()
         mean_correct.append(correct.item() / float(points.size()[0]))
 
-    class_acc[:, 2] = class_acc[:, 0] / class_acc[:, 1]
-    class_acc = np.mean(class_acc[:, 2])
     instance_acc = np.mean(mean_correct)
 
-    return instance_acc, class_acc
+    # plot 
+    cm = confusion_matrix(total_labels, total_predic)
+    plt.figure(figsize=(20, 20))
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=classes, yticklabels=classes)
+
+    # Adding labels and title
+    plt.xlabel('Predicted Labels')
+    plt.ylabel('True Labels')
+    plt.title('Confusion Matrix')
+    plt.xticks(rotation=45)
+    plt.yticks(rotation=0)  # Optional: Adjust rotation of y labels if needed
+    plt.savefig('cm.png')
+    print('confusion matrix is saved in cm.png')
+
+
+    return instance_acc
 
 
 def main(args):
-    def log_string(str):
-        logger.info(str)
-        print(str)
-
     '''HYPER PARAMETER'''
     os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu
 
-    '''CREATE DIR'''
-    timestr = str(datetime.datetime.now().strftime('%Y-%m-%d_%H-%M'))
-    exp_dir = Path('./log/')
-    exp_dir.mkdir(exist_ok=True)
-    exp_dir = exp_dir.joinpath('classification')
-    exp_dir.mkdir(exist_ok=True)
-    if args.log_dir is None:
-        exp_dir = exp_dir.joinpath(timestr)
-    else:
-        exp_dir = exp_dir.joinpath(args.log_dir)
-    exp_dir.mkdir(exist_ok=True)
-    checkpoints_dir = exp_dir.joinpath('checkpoints/')
-    checkpoints_dir.mkdir(exist_ok=True)
-    log_dir = exp_dir.joinpath('logs/')
-    log_dir.mkdir(exist_ok=True)
-
-    '''LOG'''
-    args = parse_args()
-    logger = logging.getLogger("Model")
-    logger.setLevel(logging.INFO)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    file_handler = logging.FileHandler('%s/%s.txt' % (log_dir, args.model))
-    file_handler.setLevel(logging.INFO)
-    file_handler.setFormatter(formatter)
-    logger.addHandler(file_handler)
-    log_string('PARAMETER ...')
-    log_string(args)
-
     '''DATA LOADING'''
-    log_string('Load dataset ...')
-    data_path = 'data/modelnet40_normal_resampled/'
+    print('Load dataset ...')
+    data_path = './modelnet40_normal_resampled/'
 
-    train_dataset = ModelNetDataLoader(root=data_path, args=args, split='train', process_data=args.process_data)
-    test_dataset = ModelNetDataLoader(root=data_path, args=args, split='test', process_data=args.process_data)
-    trainDataLoader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=10, drop_last=True)
+    test_dataset = ModelNetDataLoader(root=data_path, args=args, split='test', process_data=False)
     testDataLoader = torch.utils.data.DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False, num_workers=10)
 
     '''MODEL LOADING'''
-    num_class = args.num_category
-    model = importlib.import_module(args.model)
-    shutil.copy('./models/%s.py' % args.model, str(exp_dir))
-    shutil.copy('models/pointnet2_utils.py', str(exp_dir))
-    shutil.copy('./train_classification.py', str(exp_dir))
+    num_class = 40
+    model = importlib.import_module('pointnet_cls')
 
     classifier = model.get_model(num_class, normal_channel=args.use_normals)
-    criterion = model.get_loss()
-    classifier.apply(inplace_relu)
-
     if not args.use_cpu:
         classifier = classifier.cuda()
-        criterion = criterion.cuda()
 
-    try:
-        checkpoint = torch.load(str(exp_dir) + '/checkpoints/best_model.pth')
-        start_epoch = checkpoint['epoch']
-        classifier.load_state_dict(checkpoint['model_state_dict'])
-        log_string('Use pretrain model')
-    except:
-        log_string('No existing model, starting training from scratch...')
-        start_epoch = 0
+    checkpoint = torch.load(args.model)
+    classifier.load_state_dict(checkpoint['model_state_dict'])
 
-    if args.optimizer == 'Adam':
-        optimizer = torch.optim.Adam(
-            classifier.parameters(),
-            lr=args.learning_rate,
-            betas=(0.9, 0.999),
-            eps=1e-08,
-            weight_decay=args.decay_rate
-        )
-    else:
-        optimizer = torch.optim.SGD(classifier.parameters(), lr=0.01, momentum=0.9)
-
-    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.7)
-    global_epoch = 0
-    global_step = 0
-    best_instance_acc = 0.0
-    best_class_acc = 0.0
-
-    '''TRANING'''
-    logger.info('Start training...')
-    for epoch in range(start_epoch, args.epoch):
-        log_string('Epoch %d (%d/%s):' % (global_epoch + 1, epoch + 1, args.epoch))
-        mean_correct = []
-        classifier = classifier.train()
-
-        scheduler.step()
-        for batch_id, (points, target) in tqdm(enumerate(trainDataLoader, 0), total=len(trainDataLoader), smoothing=0.9):
-            optimizer.zero_grad()
-
-            points = points.data.numpy()
-            points = provider.random_point_dropout(points)
-            points[:, :, 0:3] = provider.random_scale_point_cloud(points[:, :, 0:3])
-            points[:, :, 0:3] = provider.shift_point_cloud(points[:, :, 0:3])
-            points = torch.Tensor(points)
-            points = points.transpose(2, 1)
-
-            if not args.use_cpu:
-                points, target = points.cuda(), target.cuda()
-
-            pred, trans_feat = classifier(points)
-            loss = criterion(pred, target.long(), trans_feat)
-            pred_choice = pred.data.max(1)[1]
-
-            correct = pred_choice.eq(target.long().data).cpu().sum()
-            mean_correct.append(correct.item() / float(points.size()[0]))
-            loss.backward()
-            optimizer.step()
-            global_step += 1
-
-        train_instance_acc = np.mean(mean_correct)
-        log_string('Train Instance Accuracy: %f' % train_instance_acc)
-
-        with torch.no_grad():
-            instance_acc, class_acc = test(classifier.eval(), testDataLoader, num_class=num_class)
-
-            if (instance_acc >= best_instance_acc):
-                best_instance_acc = instance_acc
-                best_epoch = epoch + 1
-
-            if (class_acc >= best_class_acc):
-                best_class_acc = class_acc
-            log_string('Test Instance Accuracy: %f, Class Accuracy: %f' % (instance_acc, class_acc))
-            log_string('Best Instance Accuracy: %f, Class Accuracy: %f' % (best_instance_acc, best_class_acc))
-
-            if (instance_acc >= best_instance_acc):
-                logger.info('Save model...')
-                savepath = str(checkpoints_dir) + '/best_model.pth'
-                log_string('Saving at %s' % savepath)
-                state = {
-                    'epoch': best_epoch,
-                    'instance_acc': instance_acc,
-                    'class_acc': class_acc,
-                    'model_state_dict': classifier.state_dict(),
-                    'optimizer_state_dict': optimizer.state_dict(),
-                }
-                torch.save(state, savepath)
-            global_epoch += 1
-
-    logger.info('End of training...')
+    with torch.no_grad():
+        instance_acc = test(classifier.eval(), testDataLoader, vote_num=args.num_votes, num_class=num_class)
+        print('Test Instance Accuracy: %f' % (instance_acc))
 
 
 if __name__ == '__main__':
